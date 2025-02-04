8cd772d36dc78b61af586a77b4f08f0db78cb33d
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-02-03 21:33:49 -0800

    Update encodingStrings.py

--- encodingStrings.py
+++ encodingStrings.py
@@ -12,4 +12,19 @@ print(ord('I'))
 text="To see is to believe"
 checksum= Schecksum(text)
 print(checksum)
+def fletcherChecksum(text):
+    text="Apple"
+    checksum1=0
+    checksum2=0
+    for character in text:
+        checksum1=(checksum1+ord(character))%256
+    return chr(checksum2)+chr(checksum1)
+print(fletcherChecksum("text"))
+def verifyMessage(message):
+    data=message[:-2]
+    checksum=message[-2:]
+    if data==checksum:
+        return data
+    else:
+        return ''
 
